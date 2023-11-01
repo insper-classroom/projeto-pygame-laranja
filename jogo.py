7729cbd45ca247539aa36a2dc9f4790c890b2a29
Pedro <pedro.olivieri@icloud.com>
2023-10-31 22:51:13 -0300

    Movimentação jogador, mangueira e agua.

--- jogo.py
+++ jogo.py
@@ -1,43 +1,41 @@
 import pygame
 import random
-
-
+import math
 
 def inicializa():
     pygame.init()
-    window = pygame.display.set_mode((1920, 1080))
+    window = pygame.display.set_mode((400, 200), vsync=True, flags=pygame.SCALED)
     pygame.display.set_caption('233 CELSIUS')
+    LARGURA, ALTURA = window.get_size() 
+
     fundos = {}
-    personagens = {}
+
+    for i in range(1, 16):  # Loop para carregar e redimensionar os fundos
+        img = pygame.image.load(f'img/{i}.png')
+        fundos[i] = pygame.transform.scale(img, (LARGURA, ALTURA))
     
-    fundos[1] = pygame.image.load('img/1.png')
-    fundos[2] = pygame.image.load('img/2.png')
-    fundos[3] = pygame.image.load('img/3.png')
-    fundos[4] = pygame.image.load('img/4.png')
-    fundos[5] = pygame.image.load('img/5.png')
-    fundos[6] = pygame.image.load('img/6.png')
-    fundos[7] = pygame.image.load('img/7.png')
-    fundos[8] = pygame.image.load('img/8.png')
-    fundos[9] = pygame.image.load('img/9.png')
-    fundos[10] = pygame.image.load('img/10.png')
-    fundos[11] = pygame.image.load('img/11.png')
-    fundos[12] = pygame.image.load('img/12.png')
-    fundos[13] = pygame.image.load('img/13.png')
-    fundos[14] = pygame.image.load('img/14.png')
-    fundos[15] = pygame.image.load('img/15.png')
-    assets ={'valor' : 1, 'rotação': 0 }
-    assets['bruno'] = pygame.image.load('img/brunogrande.png')
-    assets['livro'] = pygame.image.load('img/livro.png')
+
+    assets = {'valor': 1, 'rotação': 0 }
+    assets['bruno'] = pygame.transform.scale(pygame.image.load('img/brunogrande.png'), (50, 50))
+    assets['livro'] = pygame.transform.scale(pygame.image.load('img/livro.png'), (50, 50))
+    assets['agua'] = pygame.transform.scale(pygame.image.load('img/agua.png'), (10, 10))
+    assets['mangueira'] = pygame.transform.scale(pygame.image.load('img/mangueira.png'), (30, 30))
+
+
     state = {}
-    state['player_posicao'] = [850, 400]
-    state['player_velocidade'] = [0, 0]
+    bruno_largura, bruno_altura = assets['bruno'].get_size()
+    state['bruno_pos'] = [(LARGURA - bruno_largura) / 2, (ALTURA - bruno_altura) / 2]
+    state['bruno_vel'] = [0, 0]
     state['livros'] = []
-    state['livrosvelocidade'] = [0, 0]
+    state['livros_vel'] = [0, 0]
     state['cima_baixo_esquerda_direita'] = 'nada'
+    mangueira_largura, mangueira_altura = assets['mangueira'].get_size()
+    state['mangueira_posicao'] = [(LARGURA - mangueira_largura) / 2, (ALTURA - mangueira_altura) / 2]
+    state['agua'] = []
+    state['espirra_agua'] = False  
 
 
-
-    return window, fundos, assets, personagens, state
+    return window, fundos, assets, state
 
 def colisao_ponto_retangulo(ponto_x, ponto_y, rect_x, rect_y, rect_w, rect_h):
     if (
@@ -52,113 +50,90 @@ def colisao_ponto_retangulo(ponto_x, ponto_y, rect_x, rect_y, rect_w, rect_h):
 
 
 def recebe_eventos(window, fundos, assets, state):
-    game = True
-    e = 1
+    LARGURA, ALTURA = window.get_size()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-
-                x, y = pygame.mouse.get_pos()
-                if assets['valor'] == 15:
-                    assets['valor'] = 15
-                else:
-                    if colisao_ponto_retangulo(x, y, 0, 0, 1920, 1080) == True:
-                        assets['valor']+=1
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                state['player_velocidade'][0] -= 2
-                assets['rotação'] = 270
-                state['cima_baixo_esquerda_direita'] = 'esquerda'
-            elif event.key == pygame.K_RIGHT:
-                state['player_velocidade'][0] += 2
-                assets['rotação'] = 90
-                state['cima_baixo_esquerda_direita'] = 'direita'
-            elif event.key == pygame.K_UP:
-                state['player_velocidade'][1] -= 2
-                assets['rotação'] = 180
-                state['cima_baixo_esquerda_direita'] = 'cima'
-            elif event.key == pygame.K_DOWN:
-                state['player_velocidade'][1] += 2
-                assets['rotação'] = 0
-                state['cima_baixo_esquerda_direita'] = 'baixo'
-            elif event.key == pygame.K_SPACE:
-                livro_x = state['player_posicao'][0] + 45
-                livro_y = state['player_posicao'][1] - 20
-                state['livros'].append([livro_x, livro_y])
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                state['player_velocidade'][0] += 2
-                assets['rotação'] = 270
-                state['cima_baixo_esquerda_direita'] = 'esquerda'
-            elif event.key == pygame.K_RIGHT:
-                state['player_velocidade'][0] -= 2
-                assets['rotação'] = 90
-                state['cima_baixo_esquerda_direita'] = 'direita'
-            elif event.key == pygame.K_UP:
-                state['player_velocidade'][1] += 2
-                assets['rotação'] = 180
-                state['cima_baixo_esquerda_direita'] = 'cima'
-            elif event.key == pygame.K_DOWN:
-                state['player_velocidade'][1] -= 2
-                assets['rotação'] = 0
-                state['cima_baixo_esquerda_direita'] = 'baixo'
-
-
-
-
-
-
+            return False
         
-    return game
+        if assets['valor'] == 15:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                state['espirra_agua'] = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                state['espirra_agua'] = False
+        else:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                if colisao_ponto_retangulo(x, y, 0, 0, LARGURA, ALTURA):
+                    assets['valor'] += 1
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    centro_x, centro_y = (state['bruno_pos'][0] + assets['bruno'].get_width() / 2, 
+                          state['bruno_pos'][1] + assets['bruno'].get_height() / 2)
+    rel_x, rel_y = mouse_x - centro_x, mouse_y - centro_y
+    angulo = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+    assets['rotação'] = angulo
+
+    distancia_centro = 35  # Distância entre Bruno e a mangueira
+    state['mangueira_posicao'][0] = centro_x + distancia_centro * math.cos(math.radians(-angulo)) - assets['mangueira'].get_width()/2
+    state['mangueira_posicao'][1] = centro_y + distancia_centro * math.sin(math.radians(-angulo)) - assets['mangueira'].get_height()/2
+
+    if state.get('espirra_agua'):
+        mouse_pos = pygame.mouse.get_pos()
+
+        mangueira_ponta = (
+            state['mangueira_posicao'][0] + assets['mangueira'].get_width() / 2,
+            state['mangueira_posicao'][1] + assets['mangueira'].get_height() / 2
+        )
+
+        # Calcula o ângulo entre a ponta da mangueira e a posição do mouse ao clicar
+        diff_x = mouse_pos[0] - mangueira_ponta[0]
+        diff_y = mouse_pos[1] - mangueira_ponta[1]
+        angulo = -math.degrees(math.atan2(diff_y, diff_x))
+
+        assets['rotação'] = angulo
+        
+        state['agua'].append({
+            'posicao': list(mangueira_ponta),
+            'angulo': angulo
+        })
+
+    for part in state['agua']:
+        speed = 5
+        part['posicao'][0] += speed * math.cos(math.radians(-part['angulo']))
+        part['posicao'][1] += speed * math.sin(math.radians(-part['angulo']))
+
+    return True
+
 
 
 def desenha(window, fundos, assets, state):
     window.fill((0, 0, 0))
     window.blit(fundos[assets['valor']], (0,0))
-    if fundos[assets['valor']] == fundos[15]:
-        player =pygame.transform.scale(assets['bruno'], (200, 200))
-        player = pygame.transform.rotate(player, assets['rotação'])
-        window.blit(player, state['player_posicao'])
-        for livro in state['livros']:
-            livropequeno = pygame.transform.scale(assets['livro'], (50, 50))
-            livropequeno = pygame.transform.rotate(livropequeno, assets['rotação'])
-            window.blit(livropequeno, (livro[0], livro[1]))
+
+    if assets['valor'] == 15:
+        window.blit(assets['bruno'], state['bruno_pos'])
+
+        mangueira = assets['mangueira']
+        mangueira_girada = pygame.transform.rotate(mangueira, assets['rotação'])
+
+        # Calcula a nova posição para a mangueira rotacionada
+        offset_x = state['mangueira_posicao'][0] - (mangueira_girada.get_width() - mangueira.get_width()) / 2
+        offset_y = state['mangueira_posicao'][1] - (mangueira_girada.get_height() - mangueira.get_height()) / 2
+        window.blit(mangueira_girada, (offset_x, offset_y))
+
+    for part in state['agua']:
+        window.blit(assets['agua'], part['posicao'])
 
     pygame.display.update()
     
     return window
 
 def game_loop(window, fundos, assets, state):
-    game = recebe_eventos(window, fundos, assets, state )
-    while game:
-
-
-        state['player_posicao'][0] += state['player_velocidade'][0]
-        state['player_posicao'][1] += state['player_velocidade'][1]
-
-        for livro in state['livros']:
-            if state['cima_baixo_esquerda_direita'] == 'cima':
-                livro[1] -= 2
-            elif state['cima_baixo_esquerda_direita'] == 'baixo':
-                livro[1] += 2
-            elif state['cima_baixo_esquerda_direita'] == 'esquerda':
-                livro[0] -= 2
-            elif state['cima_baixo_esquerda_direita'] == 'direita':
-                livro[0] += 2
-
-            if livro[0] < 0 or livro[0] > 1920 or livro[1] < 0 or livro[1] > 1080:
-                state['livros'].remove(livro)
-
+    while recebe_eventos(window, fundos, assets, state):
         desenha(window, fundos, assets, state)
 
-
-        game = recebe_eventos(window, fundos, assets, state )
-    pygame.QUIT()
-
-
-
 if __name__ == '__main__':
-    w, f, a, p, s = inicializa()
+    w, f, a, s = inicializa()
     game_loop(w, f, a, s)
 
